172359e000648c9c734591073ac1d564d92d1500
90felipe09 <Felhipeshiraishi@gmail.com>
2021-07-17 16:18:45 -0300

    add: payfluxo notification and messages

--- src/base/payfluxo/payfluxoservice.h
+++ src/base/payfluxo/payfluxoservice.h
@@ -10,18 +10,22 @@ private:
     QWebSocket m_webSocket;
     void sendMessage(QString message);
 
+    void handlePaymentNotification(QString ip);
+    void handleIntentionDeclaredNotification(QString torrentIdString, int status);
+    void handleWalletNotification(float newAvailable, float newFrozen, float newRedeemable);
+
 public:
     explicit PayfluxoService(bool debug, QObject* parent);
     void onConnected();
     void closed();
     void onWebSocketError(QAbstractSocket::SocketError error);
-    void handlePaymentNotification(QString ip);
-    void handleIntentionDeclaredNotification(QString torrentIdString, int status);
     void onTextMessageReceived(QString message);
     void sendBlockDownloadedMessage(QString ip, QString torrentId, QString fileSize);
     void sendAuthenticatedMessage(QString certificate, QString privateKey, QString orgMSP);
     void sendDeauthMessage();
     void sendDownloadIntentionMessage(QString magneticLink, int piecesNumber, QString torrentId);
     void sendCloseMessage();
+    void sendRedeemMessage();
+    void sendRefreshWalletMessage();
 };
 #endif
